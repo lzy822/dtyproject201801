be55513c6165068ef851e620fd8ba88da3afd379
Zhengyang <542865759@qq.com>
2018-03-15 17:33:55 +0800

    数据库配置, 设计

---
+++ app/src/main/java/com/geopdfviewer/android/PhotoLoc.java
@@ -0,0 +1,53 @@
+package com.geopdfviewer.android;
+
+/**
+ * Created by 54286 on 2018/3/15.
+ */
+
+public class PhotoLoc {
+    private int id;
+    private String name;
+    private String path;
+    private float x;
+    private float y;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+}
---
+++ app/src/main/java/com/geopdfviewer/android/Trail.java
@@ -0,0 +1,37 @@
+package com.geopdfviewer.android;
+
+import java.util.List;
+
+/**
+ * Created by 54286 on 2018/3/15.
+ */
+
+public class Trail {
+    private int id;
+    private String name;
+    private String path;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
